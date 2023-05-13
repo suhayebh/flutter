fca1365815597e9d9b7fa1f536be73f4453ca954
suhayebh <one@suhayeb.com>
2023-05-13 18:48:47 +0400

    course complete

---
+++ chat_app/lib/screens/chat.dart
@@ -0,0 +1,77 @@
+import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_message.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+  void setupPushNotifications()  async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+
+    final token = await fcm.getToken();
+    print ('token: $token');
+    // fcm.configure(
+    //   onMessage: (msg) {
+    //     print(msg);
+    //     return;
+    //   },
+    //   onLaunch: (msg) {
+    //     print(msg);
+    //     return;
+    //   },
+    //   onResume: (msg) {
+    //     print(msg);
+    //     return;
+    //   },
+    // );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setupPushNotifications();
+    // FirebaseAuth.instance.authStateChanges().listen((user) {
+    //   if (user == null) {
+    //     Navigator.of(context).pushReplacementNamed('/');
+    //   }
+    // });
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FlutterChat'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: const [
+          Expanded(
+            child: ChatMessages(),
+          ),
+          NewMessage(),
+        ],
+      ),
+    );
+  }
+}
